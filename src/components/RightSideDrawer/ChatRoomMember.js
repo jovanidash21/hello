159966cbdcecc8a85b06af706ab9a181252e1a6e
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-05-17 17:20:40 +0800

    Update Right Side Drawer Chat Room Member Component

--- src/components/RightSideDrawer/ChatRoomMember.js
+++ src/components/RightSideDrawer/ChatRoomMember.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FontAwesome from 'react-fontawesome';
 import './styles.scss';
 
 const ChatRoomMember = (props) => {
@@ -12,6 +13,29 @@ const ChatRoomMember = (props) => {
       <div className="member-name">
         {props.chatRoomMember.name}
       </div>
+      <div className="member-options-button" data-mui-toggle="dropdown">
+        <FontAwesome
+          className="options-icon"
+           name="ellipsis-v"
+        />
+      </div>
+      <ul className="mui-dropdown__menu mui-dropdown__menu--right">
+        <li>
+          <a href="#">Make Admin</a>
+        </li>
+        <li>
+          <a href="#">Ban Member</a>
+        </li>
+        <li>
+          <a href="#">Kick Member</a>
+        </li>
+        <li>
+          <a href="#">Mute Member</a>
+        </li>
+        <li>
+          <a href="#">Make VIP</a>
+        </li>
+      </ul>
     </div>
   )
 }
