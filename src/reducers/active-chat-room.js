f5e91d0d8181dd6e944085b1474a5f1fb14ed2dd
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-05-28 16:48:19 +0800

    Update Kick Member Option

--- src/reducers/active-chat-room.js
+++ src/reducers/active-chat-room.js
@@ -1,3 +1,4 @@
+import { SOCKET_BROADCAST_KICK_USER } from '../constants/user';
 import { CHANGE_CHAT_ROOM } from '../constants/active-chat-room';
 
 const initialState = {
@@ -6,6 +7,17 @@ const initialState = {
 
 const activeChatRoom = (state=initialState, action) => {
   switch(action.type) {
+    case SOCKET_BROADCAST_KICK_USER:
+      var chatRoomID = action.chatRoom;
+      var chatRoomData = {...state.chatRoomData};
+
+      if ( chatRoomData._id === chatRoomID ) {
+        location.reload();
+      }
+
+      return {
+        ...state
+      }
     case CHANGE_CHAT_ROOM:
       return {
         chatRoomData: action.payload
