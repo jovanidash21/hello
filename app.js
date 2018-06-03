f56575311b54ab2b5612ac9f6a9138a3b239444b
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-06-03 21:57:11 +0800

    Add Express IP Filter Config

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ var api = require('./routes/api');
 
 var app = express();
 
+// ip blacklist
+var fs = require('fs');
+var ipfilter = require('express-ipfilter').IpFilter;
+var ipBlacklist = fs.readFileSync('ip-blacklist.txt').toString().replace(/\r\n/g,'\n').split('\n');
+
+app.use(ipfilter(ipBlacklist));
+
 // browserSync setup
 if ( process.env.NODE_ENV != 'production' ) {
   var browserSync = require('browser-sync');
