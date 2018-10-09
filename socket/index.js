2a90e223b7d5eaf549f44e0f818979205ef64c5b
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-10-09 12:49:43 +0800

    Update Socket.IO Events

--- socket/index.js
+++ socket/index.js
@@ -20,17 +20,18 @@ var sockets = function(io) {
                 socketID: socket.id
               }
             }, { safe: true, upsert: true, new: true },
-            function(err, user) {
-              if (!err) {
-                users[socket.id] = user._id;
+          )
+          .then((user) => {
+            users[socket.id] = action.user._id;
 
-                socket.broadcast.emit('action', {
-                  type: 'SOCKET_BROADCAST_USER_LOGIN',
-                  user: user
-                });
-              }
-            }
-          );
+            socket.broadcast.emit('action', {
+              type: 'SOCKET_BROADCAST_USER_LOGIN',
+              user: user
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
           break;
         case 'SOCKET_JOIN_CHAT_ROOM':
           socket.join(action.chatRoomID);
@@ -39,16 +40,20 @@ var sockets = function(io) {
           socket.leave(action.chatRoomID);
           break;
         case 'SOCKET_CREATE_CHAT_ROOM':
-          action.members.forEach(function (chatRoomMember) {
-            User.findById(chatRoomMember, function(err, user) {
-              if (!err) {
+          for (var i = 0; i < action.members.length; i++) {
+            var chatRoomMember = action.members[i];
+
+            User.findById(chatRoomMember)
+              .then((user) => {
                 socket.broadcast.to(user.socketID).emit('action', {
                   type: 'SOCKET_BROADCAST_CREATE_CHAT_ROOM',
                   chatRoom: action.chatRoomBroadcast
                 });
-              }
-            });
-          });
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          }
           break;
         case 'SOCKET_SEND_MESSAGE':
           var chatRoomClients = [];
@@ -61,63 +66,60 @@ var sockets = function(io) {
 
           ChatRoom.findById(action.chatRoomID)
             .populate('members')
-            .exec(function(err, chatRoom) {
-              if (!err) {
-                for (var i = 0; i < chatRoom.members.length; i++) {
-                  var chatRoomMember = chatRoom.members[i];
+            .exec()
+            .then((chatRoom) => {
+              for (var i = 0; i < chatRoom.members.length; i++) {
+                var chatRoomMember = chatRoom.members[i];
 
-                  User.findOneAndUpdate(
-                    { _id: chatRoomMember, 'chatRooms.data': action.chatRoomID },
-                    { $set: { 'chatRooms.$.trash.data': false, 'chatRooms.$.trash.endDate': new Date() } },
-                    { safe: true, upsert: true, new: true },
-                    function(err, user) {
-                      if (!err) {
-                        if (chatRoomClients.indexOf(user.socketID) > -1) {
-                          Message.findOneAndUpdate(
-                            { _id: action.message._id, readBy: { $ne: user._id } },
-                            { $addToSet: { readBy: user._id } },
-                            { safe: true, upsert: true, new: true },
-                            function(err) {
-                              if (!err) {
-                                User.update(
-                                  { _id: user._id, 'chatRooms.data': action.chatRoomID },
-                                  { $set: { 'chatRooms.$.unReadMessages': 0 } },
-                                  { safe: true, upsert: true, new: true },
-                                  function(err) {
-                                    if (!err) {
-                                      socket.broadcast.to(user.socketID).emit('action', {
-                                        type: 'SOCKET_BROADCAST_SEND_MESSAGE',
-                                        message: action.message
-                                      });
-                                    }
-                                  }
-                                );
-                              }
-                            }
-                          );
-                        } else {
-                          if (chatRoom.chatType === 'direct') {
-                            chatRoom.name = action.message.user.name;
+                User.findOneAndUpdate(
+                  { _id: chatRoomMember, 'chatRooms.data': action.chatRoomID },
+                  { $set: { 'chatRooms.$.trash.data': false, 'chatRooms.$.trash.endDate': new Date() } },
+                  { safe: true, upsert: true, new: true },
+                )
+                .then((user) => {
+                  if (chatRoomClients.indexOf(user.socketID) > -1) {
+                    Message.findOneAndUpdate(
+                      { _id: action.message._id, readBy: { $ne: user._id } },
+                      { $addToSet: { readBy: user._id } },
+                      { safe: true }
+                    ).exec();
 
-                            socket.broadcast.to(user.socketID).emit('action', {
-                              type: 'SOCKET_BROADCAST_CREATE_CHAT_ROOM',
-                              chatRoom: {data: chatRoom, unReadMessages: 0},
-                            });
+                    User.update(
+                      { _id: user._id, 'chatRooms.data': action.chatRoomID },
+                      { $set: { 'chatRooms.$.unReadMessages': 0 } },
+                      { safe: true, upsert: true, new: true }
+                    ).exec();
 
-                            socket.broadcast.to(user.socketID).emit('action', {
-                              type: 'SOCKET_BROADCAST_NOTIFY_MESSAGE',
-                              chatRoom: {data: chatRoom, unReadMessages: 0},
-                              chatRoomID: action.chatRoomID,
-                              chatRoomName: chatRoom.name,
-                              senderName: action.message.user.name
-                            });
-                          }
-                        }
-                      }
+                    socket.broadcast.to(user.socketID).emit('action', {
+                      type: 'SOCKET_BROADCAST_SEND_MESSAGE',
+                      message: action.message
+                    });
+                  } else {
+                    if (chatRoom.chatType === 'direct') {
+                      chatRoom.name = action.message.user.name;
+
+                      socket.broadcast.to(user.socketID).emit('action', {
+                        type: 'SOCKET_BROADCAST_CREATE_CHAT_ROOM',
+                        chatRoom: {data: chatRoom, unReadMessages: 0},
+                      });
+
+                      socket.broadcast.to(user.socketID).emit('action', {
+                        type: 'SOCKET_BROADCAST_NOTIFY_MESSAGE',
+                        chatRoom: {data: chatRoom, unReadMessages: 0},
+                        chatRoomID: action.chatRoomID,
+                        chatRoomName: chatRoom.name,
+                        senderName: action.message.user.name
+                      });
                     }
-                  );
-                }
+                  }
+                })
+                .catch((error) => {
+                  console.log(error);
+                });
               }
+            })
+            .catch((error) => {
+              console.log(error);
             });
           break;
         case 'SOCKET_DELETE_MESSAGE':
@@ -156,8 +158,8 @@ var sockets = function(io) {
             });
           break;
         case 'SOCKET_BLOCK_MEMBER':
-          User.findById(action.memberID, function(err, user) {
-            if (!err) {
+          User.findById(action.memberID)
+            .then((user) => {
               socket.broadcast.to(user.socketID).emit('action', {
                 type: 'SOCKET_BROADCAST_BLOCK_USER',
                 chatRoomID: action.chatRoomID
@@ -168,12 +170,14 @@ var sockets = function(io) {
                 chatRoomID: action.chatRoomID,
                 memberID: action.memberID
               });
-            }
-          });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
           break;
         case 'SOCKET_KICK_MEMBER':
-          User.findById(action.memberID, function(err, user) {
-            if (!err) {
+          User.findById(action.memberID)
+            .then((user) => {
               socket.broadcast.to(user.socketID).emit('action', {
                 type: 'SOCKET_BROADCAST_KICK_USER',
                 chatRoomID: action.chatRoomID
@@ -184,8 +188,10 @@ var sockets = function(io) {
                 chatRoomID: action.chatRoomID,
                 memberID: action.memberID
               });
-            }
-          });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
           break;
         case 'SOCKET_UPDATE_MEMBER_ROLE':
           socket.broadcast.emit('action', {
@@ -195,14 +201,16 @@ var sockets = function(io) {
           });
           break;
         case 'SOCKET_MUTE_MEMBER':
-          User.findById(action.memberID, function(err, user) {
-            if (!err) {
+          User.findById(action.memberID)
+            .then((user) => {
               socket.broadcast.emit('action', {
                 type: 'SOCKET_BROADCAST_MUTE_MEMBER',
                 memberID: action.memberID
               });
-            }
-          });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
           break;
         default:
           break;
@@ -213,17 +221,18 @@ var sockets = function(io) {
         users[socket.id],
         { $set: { isOnline: false, ipAddress: '', socketID: ''} },
         { safe: true, upsert: true, new: true },
-        function(err) {
-          if (!err) {
-            socket.broadcast.emit('action', {
-              type: 'SOCKET_BROADCAST_USER_LOGOUT',
-              userID: users[socket.id]
-            });
+      )
+      .then((user) => {
+        socket.broadcast.emit('action', {
+          type: 'SOCKET_BROADCAST_USER_LOGOUT',
+          userID: users[socket.id]
+        });
 
-            delete users[socket.id];
-          }
-        }
-      );
+        delete users[socket.id];
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     });
   });
 };
