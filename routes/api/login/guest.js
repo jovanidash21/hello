c315cc27afcc57529a4242c6cfbb72d07d325000
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-10-15 22:28:53 +0800

    Update Guest Login API

--- routes/api/login/guest.js
+++ routes/api/login/guest.js
@@ -22,49 +22,66 @@ router.post('/', function(req, res, next) {
     username: req.body.username,
     name: req.body.name,
     gender: req.body.gender,
-    accountType: 'guest',
-    role: 'ordinary',
-    block: {},
-    mute: {}
+    accountType: 'guest'
   };
-  var newUser = new User(userData);
 
-  newUser.save(function(err) {
+  User.findOne({name: req.body.name, accountType: 'guest'}, function(err, user) {
     if (!err) {
-      passport.authenticate('custom', function(err, user) {
-        req.logIn(user, function(err) {
+      if (user !== null) {
+        res.status(401).send({
+          success: false,
+          message: 'Sorry! Guest name already taken'
+        });
+      } else {
+        userData.role = 'ordinary';
+        userData.block = {};
+        userData.mute = {};
+        var newUser = new User(userData);
+
+        newUser.save(function(err) {
           if (!err) {
-            var userID = newUser._id;
+            passport.authenticate('custom', function(err, user) {
+              req.logIn(user, function(err) {
+                if (!err) {
+                  var userID = newUser._id;
 
-            ChatRoom.find({_id: {$ne: null}, chatType: 'public'})
-              .distinct('_id')
-              .then((publicChatRooms) => {
-                for (var i = 0; i < publicChatRooms.length; i++) {
-                  var publicChatRoomID = publicChatRooms[i];
+                  ChatRoom.find({_id: {$ne: null}, chatType: 'public'})
+                    .distinct('_id')
+                    .then((publicChatRooms) => {
+                      for (var i = 0; i < publicChatRooms.length; i++) {
+                        var publicChatRoomID = publicChatRooms[i];
 
-                  ChatRoom.findByIdAndUpdate(
-                    publicChatRoomID,
-                    { $push: { members: userID }},
-                    { safe: true, upsert: true, new: true }
-                  ).exec();
+                        ChatRoom.findByIdAndUpdate(
+                          publicChatRoomID,
+                          { $push: { members: userID }},
+                          { safe: true, upsert: true, new: true }
+                        ).exec();
 
-                  User.findByIdAndUpdate(
-                    userID,
-                    { $push: { chatRooms: { data: publicChatRoomID, unReadMessages: 0, kick: {}, trash: {} } } },
-                    { safe: true, upsert: true, new: true }
-                  ).exec();
+                        User.findByIdAndUpdate(
+                          userID,
+                          { $push: { chatRooms: { data: publicChatRoomID, unReadMessages: 0, kick: {}, trash: {} } } },
+                          { safe: true, upsert: true, new: true }
+                        ).exec();
+                      }
+
+                      res.end();
+                    })
+                    .catch((error) => {
+                      res.end(err);
+                    });
+                } else {
+                  res.end(err);
                 }
-
-                res.end();
-              })
-              .catch((error) => {
-                res.end(err);
               });
+            })(req, res, next);
           } else {
-            res.end(err);
+            res.status(500).send({
+              success: false,
+              message: 'Server Error!'
+            });
           }
         });
-      })(req, res, next);
+      }
     } else {
       res.status(500).send({
         success: false,
