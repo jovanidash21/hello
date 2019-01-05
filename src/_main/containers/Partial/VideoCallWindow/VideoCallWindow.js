653da159a6182682bf566429fa48c3c3a027a0b1
Jovani Cadorna Warguez <jwardash21@gmail.com>
2019-01-05 22:59:05 +0800

    Update Video Call Window Container

--- src/_main/containers/Partial/VideoCallWindow/VideoCallWindow.js
+++ src/_main/containers/Partial/VideoCallWindow/VideoCallWindow.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Webcam from 'react-webcam';
 import FontAwesome from 'react-fontawesome';
 import mapDispatchToProps from '../../../actions';
 import './styles.scss';
@@ -17,18 +18,29 @@ class VideoCallWindow extends Component {
     handleCloseWindow();
   }
   render() {
+    const videoConstraints = {
+      height: {
+        min: 360,
+        ideal: 720,
+        max: 1080
+      },
+      facingMode: "user"
+    };
+
     return (
       <div className="video-call-window">
-        <video className="remote-video" ref={(element) => { this.remoteVideo = element; }} autoPlay loop>
-          <source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4" />
-        </video>
-        <video className="local-video" ref={(element) => { this.localVideo = element; }} autoPlay muted loop>
-          <source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4" />
-        </video>
+        <Webcam
+          className="remote-video"
+          videoConstraints={videoConstraints}
+          ref={this.remoteVideo}
+          audio={false}
+        />
+        <Webcam
+          className="local-video"
+          videoConstraints={videoConstraints}
+          ref={this.localVideo}
+        />
         <div className="video-call-controls">
-          <div className="video-call-button accept-call-button" title="Accept Video Call">
-            <FontAwesome name="video-camera" size="2x" />
-          </div>
           <div className="video-call-button end-call-button" title="Reject Video Call" onClick={::this.handleCloseWindow}>
             <FontAwesome name="phone" size="2x" />
           </div>
